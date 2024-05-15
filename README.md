f55ea982c2f5fe582894292e1156feed33cedad3
dza59 <zdn.zhang@gmail.com>
2024-05-14 22:02:42 -0700

    add  readme & rename folder

---
+++ README.md
@@ -0,0 +1,53 @@
+## <a name="introduction">🤖 Introduction</a>
+
+My friend is a photographer who uses a camera model that doesn't sync JPG and RAW files accordingly. She wants to sync all RAW files with keeper JPEGs from a shoot, but it takes a lot of time manually comparing both folders and deleting files. To help her out, I created two versions of a solution:
+
+**_Streamlit Version:_** This version allows her to use a web browser on her local machine to specify the desired root folder.
+**_Bash Script Version:_** This version is a standalone Python script that runs automatically using a bash file.
+
+I was able to create these simple scripts within 3 hours, saving her hours of work every week.
+
+###### Technology saves our lives so we can keep rolling the ball!
+
+## <a name="tech-stack">⚙️ Tech Stack</a>
+
+- Python
+- Streamlit
+
+## <a name="quick-start">🤸 Quick Start</a>
+
+Follow these steps to set up the Streamlit project locally on your machine.
+
+**Prerequisites**
+
+Make sure you have the following installed on your machine:
+
+- [Git](https://git-scm.com/)
+- [Python](https://www.python.org)
+- [Streamlit](https://docs.streamlit.io/get-started/installation)
+- [pip](https://pypi.org/project/pip/)
+
+**Cloning the Repository**
+
+```bash
+git clone git@github.com:dza59/photo-sync-tool.git
+cd photo-sync-tool
+```
+
+**Installation**
+
+Install the project dependencies use pip pr pip3
+
+```bash
+pip3 install -r requirements.txt
+```
+
+**Running the Project**
+
+```bash
+streamlit run app.py
+```
+
+<h2 style="text-align: center;">
+❤️ ❤️ Thank you for reading! ❤️ ❤️
+</h2>
---
+++ script/sync_folders.py
@@ -0,0 +1,72 @@
+import os
+from pathlib import Path
+import shutil
+
+
+def sync_folders(main_folder):
+    jpg_folder = main_folder / 'jpg'
+    raw_folder = main_folder / 'Raw'
+
+    if not jpg_folder.exists() or not raw_folder.exists():
+        return "💔  The 'jpg' and/or 'Raw' folders do not exist in the specified directory.", [], []
+
+    temp_folder = main_folder / 'temp'
+
+    # loop all JPG and CR3 files
+    jpg_files = {f.stem: f for f in jpg_folder.glob('*.JPG')}
+    raw_files = {f.stem: f for f in raw_folder.glob('*.CR3')}
+
+    # get unmatched files
+    unmatched_jpg = jpg_files.keys() - raw_files.keys()
+    unmatched_raw = raw_files.keys() - jpg_files.keys()
+
+    if not unmatched_jpg and not unmatched_raw:
+        return "💃 JPG and RAW photos are already synced. No sync needed 🕺🏼 🎉", [], []
+
+    return "⚠️ ⚠️ 💔 Unmatched files detected. Please review before final deletion.", list(unmatched_jpg), list(unmatched_raw)
+
+
+def move_files_to_temp(main_folder, unmatched_jpg, unmatched_raw):
+    temp_folder = main_folder / 'temp'
+    temp_folder.mkdir(exist_ok=True)
+
+    jpg_folder = main_folder / 'jpg'
+    raw_folder = main_folder / 'Raw'
+
+    # move unmatched files to a temp folder
+    for file_stem in unmatched_jpg:
+        shutil.move(str(jpg_folder / (file_stem + '.JPG')), str(temp_folder))
+    for file_stem in unmatched_raw:
+        shutil.move(str(raw_folder / (file_stem + '.CR3')), str(temp_folder))
+
+
+def main():
+    main_folder = Path(__file__).parent
+    result, unmatched_jpg, unmatched_raw = sync_folders(main_folder)
+    print(result)
+
+    if unmatched_jpg or unmatched_raw:
+        if unmatched_jpg:
+            print("Unmatched JPG files:")
+            for jpg in unmatched_jpg:
+                print(f" - {jpg}.JPG")
+        if unmatched_raw:
+            print("Unmatched RAW files:")
+            for raw in unmatched_raw:
+                print(f" - {raw}.CR3")
+
+        delete = input(
+            "💀 ⚠️  Do you want to delete the unmatched files? (yes/no, press Enter for yes): ").strip().lower()
+        if delete == 'yes' or delete == '':
+            move_files_to_temp(main_folder, unmatched_jpg, unmatched_raw)
+            temp_folder = main_folder / 'temp'
+            shutil.rmtree(temp_folder)
+            print("🎉 🎉 folders synced successfully. 🎉 🎉")
+        else:
+            print("⚠️ Review the unmatched files in the 'temp' folder before deletion.")
+    else:
+        print("💃 🕺🏼 🎉 No unmatched files to delete. 🎉")
+
+
+if __name__ == "__main__":
+    main()
